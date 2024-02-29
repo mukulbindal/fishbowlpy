f1ec3f9091a7c671a9bc4c57548dad4bf26edae7
Jayme Klein <jayme.silva@winddigital.com.br>
2024-02-29 16:23:02 +0000

    Enumerated the drivers on drivertype.py; Imported and created default installations for Firefox and IE.
    I've not finished Opera and Brave drivers / services

--- src/fishbowlpy/browserdriver.py
+++ src/fishbowlpy/browserdriver.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome import service
+from selenium.webdriver.chrome.service import Service as ChromiumService
 from selenium.webdriver.chromium.webdriver import ChromiumDriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import IEDriverManager
+from webdriver_manager.opera import OperaDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 
 from .drivertype import DriverType
 
@@ -11,8 +17,8 @@ LOGGER = getLogger(__name__)
 
 
 class BrowserDriver:
-    
-    def __init__(self, driver_type=DriverType.CHROME_DRIVER, driver_path:str=None) -> None:
+
+    def __init__(self, driver_type=DriverType.CHROME_DRIVER, driver_path: str = None) -> None:
         self.__driver = None
         self.__service = None
         self.__driver_type = driver_type or DriverType.CHROME_DRIVER
@@ -20,21 +26,58 @@ class BrowserDriver:
 
         if driver_type == DriverType.CHROME_DRIVER:
             self.__service = webdriver.ChromeService(
-                executable_path=self.__driver_path)
+                    executable_path=self.__driver_path)
             self.__driver = webdriver.Chrome(service=self.__service)
+
         elif driver_type == DriverType.EDGE_DRIVER:
             self.__service = webdriver.EdgeService(
-                executable_path=self.__driver_path)
+                    executable_path=self.__driver_path)
             self.__driver = webdriver.Edge(service=self.__service)
+
+        elif driver_type == DriverType.FIREFOX_DRIVER:
+            self.__service = webdriver.FirefoxService(
+                    executable_path=self.__driver_path)
+            self.__driver = webdriver.Firefox(service=self.__service)
+
+        elif driver_type == DriverType.IE_DRIVER:
+            self.__service = webdriver.IeService(
+                    executable_path=self.__driver_path)
+            self.__driver = webdriver.Ie(service=self.__service)
+
+        elif driver_type == DriverType.OPERA_DRIVER:
+            self.__service = service.Service(
+                    executable_path=self.__driver_path)
+            # TODO: self.__driver = webdriver.Opera(service=self.__service)
+
+        elif driver_type == DriverType.BRAVE_DRIVER:
+            pass
+
+            # TODO: Must do the BraveDriver
 
     def get_driver(self) -> ChromiumDriver:
         return self.__driver
 
-    def get_default_driver(self, driver_type) -> ChromiumDriver:
+    def get_default_driver(self, driver_type: int) -> ChromiumDriver:
         try:
-            if driver_type == DriverType.CHROME_DRIVER:
-                return ChromeDriverManager().install()
-            elif driver_type == DriverType.EDGE_DRIVER:
-                return EdgeChromiumDriverManager().install()
+            match driver_type:
+                case DriverType.CHROME_DRIVER:
+                    return ChromeDriverManager().install()
+
+                case DriverType.EDGE_DRIVER:
+                    return EdgeChromiumDriverManager().install()
+
+                case DriverType.FIREFOX_DRIVER:
+                    return GeckoDriverManager().install()
+
+                case DriverType.IE_DRIVER:
+                    return IEDriverManager().install()
+
+                case DriverType.OPERA_DRIVER:
+                    return OperaDriverManager().install()
+
+                case DriverType.BRAVE_DRIVER:
+                    return ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()
+
+
         except Exception as e:
             LOGGER.error(f"Error getting default driver {e}")
